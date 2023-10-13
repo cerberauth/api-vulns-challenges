a6843cadbcf7539e03d33c21c39124916671219e
Emmanuel Gautier <git@emmanuelgautier.fr>
2023-10-13 19:40:42 +0200

    feat: add challenge with strong key and no fake vuln

---
+++ challenges/jwt-strong-eddsa-key/cmd/jwt/root.go
@@ -0,0 +1,52 @@
+package jwt
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/spf13/cobra"
+)
+
+func GenerateRS512JWT(sub string) (string, error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	privateKeyBytes, err := os.ReadFile(cwd + string(os.PathSeparator) + "keys/private_key.pem")
+	if err != nil {
+		return "", err
+	}
+
+	key, err := jwt.ParseEdPrivateKeyFromPEM(privateKeyBytes)
+	if err != nil {
+		return "", err
+	}
+
+	tokenString, err := jwt.NewWithClaims(jwt.SigningMethodEdDSA, jwt.MapClaims{
+		"sub": sub,
+	}).SignedString(key)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func NewJwtCmd() (jwtCmd *cobra.Command) {
+	jwtCmd = &cobra.Command{
+		Use: "jwt",
+		Run: func(cmd *cobra.Command, args []string) {
+			tokenString, err := GenerateRS512JWT("abc123")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Printf("token: %s", tokenString)
+		},
+	}
+
+	return jwtCmd
+}
---
+++ challenges/jwt-strong-eddsa-key/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/cerberauth/api-vulns-challenges/challenges/jwt-strong-eddsa-key/cmd"
+
+func main() {
+	cmd.Execute()
+}
