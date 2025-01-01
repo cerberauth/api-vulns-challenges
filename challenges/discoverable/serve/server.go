784f0e7be2938a625b3e0f79985b35731d0529a6
Emmanuel Gautier <git@emmanuelgautier.fr>
2025-01-01 21:49:52 +0100

    feat: add discoverable healthcheck endpoints

--- challenges/discoverable/serve/server.go
+++ challenges/discoverable/serve/server.go
@@ -9,6 +9,13 @@ func RunServer(port string) {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
+	http.HandleFunc("/health/ready", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+	http.HandleFunc("/health/live", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
 	log.Println("Server started at port", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
